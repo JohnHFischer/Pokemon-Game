e143d0941bc1f6e627b44a236bcca702e29cecb3
JohnHFischer <46055329+JohnHFischer@users.noreply.github.com>
2019-01-11 19:59:53 -0500

    Add files via upload

---
+++ Item.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Project
+{
+    public class Item
+    {
+        public string name;      
+        public string description;
+        public Image[] itemImage = { Properties.Resources.boatImage, 
+                                   Properties.Resources.exitImage, 
+                                   Properties.Resources.healthPotion};
+        public int x, y;
+        public int imageNumber;
+        public int price;
+    
+        public Item()
+        {
+        }
+
+        public Item(string _name, string _description, Image[] _itemImage, int _imageNumber, int _x, int _y, int _price)
+        {
+            name = _name;          
+            description = _description;
+            itemImage = _itemImage;
+            imageNumber = _imageNumber;
+            x = _x;
+            y = _y;
+            price = _price;           
+        }
+    }
+}
---
+++ Tree.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Project
+{
+    public class Tree
+    {
+        public int x;
+        public int y;
+    
+
+        public Image treeImage = Properties.Resources.tree;
+
+        public Tree()
+        {
+        }
+
+        public Tree(int _x, int _y, Image _treeImage)
+        {
+            x = _x;
+            y = _y;        
+            treeImage = _treeImage;
+
+        }
+    }
+}
+
